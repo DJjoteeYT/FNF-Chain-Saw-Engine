c0eb4641a6a6169a1e66e74464e8f95bbd16bb33
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-09-14 09:58:46 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -2,16 +2,33 @@ package;
 
 import flixel.FlxGame;
 import lime.app.Application;
+import openfl.Lib;
 import openfl.display.Sprite;
 import states.TitleState;
 
 class Main extends Sprite
 {
+	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
+
 	public function new()
 	{
 		super();
 
-		addChild(new FlxGame(0, 0, TitleState, 1, 60, 60, true, false));
+		final stageWidth:Int = Lib.current.stage.stageWidth;
+		final stageHeight:Int = Lib.current.stage.stageHeight;
+
+		if (zoom == -1)
+		{
+			var ratioX:Float = stageWidth / gameWidth;
+			var ratioY:Float = stageHeight / gameHeight;
+			zoom = Math.min(ratioX, ratioY);
+			gameWidth = Math.ceil(stageWidth / zoom);
+			gameHeight = Math.ceil(stageHeight / zoom);
+		}
+
+		addChild(new FlxGame(gameWidth, gameHeight, TitleState, zoom, 60, 60, true, false));
 		addChild(new Overlay(10, 10, 0xFFFFFF));
 
 		PlayerSettings.init();
