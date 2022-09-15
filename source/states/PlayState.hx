33629945878957ed3db8971cea059d448a7a347d
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-09-15 07:34:44 +0300

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1500,7 +1500,7 @@ class PlayState extends MusicBeatState
 		super.destroy();
 	}
 
-	private function callScripts(funcName:String, args:Array<Any>)
+	private function callScripts(funcName:String, args:Array<Dynamic>)
 		for (i in 0...scriptArray.length)
 			scriptArray[i].executeFunc(funcName, args);
 
