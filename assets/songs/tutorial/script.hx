c3e62c3ab117994e923cb66d1abecd5946f1a625
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-09-15 10:56:34 +0300

    Oops
    Signed-off-by: Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>

--- assets/songs/tutorial/script.hx
+++ assets/songs/tutorial/script.hx
@@ -1,6 +1,6 @@
 function beatHit(curBeat:Int)
 {
-	if (curBeat % 16 == 15 && PlayState.dad.curCharacter == 'gf' && curBeat > 16 && curBeat < 48)
+	if (curBeat % 16 == 15 && PlayState.instance.dad.curCharacter == 'gf' && curBeat > 16 && curBeat < 48)
 	{
 		PlayState.instance.boyfriend.playAnim('hey', true);
 		PlayState.instance.dad.playAnim('cheer', true);
