8d4cf448fbae205a9d5ee832ff5f3f158ccf81fa
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-09-16 10:13:19 +0300

    Update DialogueBox.hx
    Signed-off-by: Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -166,7 +166,15 @@ class DialogueBox extends FlxSpriteGroup
 			dialogueStarted = true;
 		}
 
-		if (FlxG.keys.justPressed.ANY && dialogueStarted == true)
+		#if android
+		var justTouched:Bool = false;
+
+		for (touch in FlxG.touches.list)
+			if (touch.justPressed)
+				justTouched = true;
+		#end
+
+		if (FlxG.keys.justPressed.ANY #if android || justTouched #end && dialogueStarted == true)
 		{
 			FlxG.sound.play(Paths.sound('clickText'), 0.8);
 
